2ca70f9a971b109c1cc9016a9ce7f08b8492a0ac
mshadloo <33298105+mshadloo@users.noreply.github.com>
2021-03-05 13:00:04 -0800

    Update coco.sh

--- coco.sh
+++ coco.sh
@@ -2,3 +2,6 @@ wget http://images.cocodataset.org/zips/val2014.zip
 unzip val2014.zip 
 rm val2014.zip
 mv val2014 coco
+mv coco  ./objectIdentificationWebApp/static
+cd objectIdentificationWebApp
+python coco_featureExtraction.py
