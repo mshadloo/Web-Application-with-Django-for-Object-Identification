3a2894ef560da63276323cdff209a893b391ccf8
mshadloo <33298105+mshadloo@users.noreply.github.com>
2021-03-05 13:33:23 -0800

    Update coco_featureExtraction.py

--- objectIdentificationWebApp/coco_featureExtraction.py
+++ objectIdentificationWebApp/coco_featureExtraction.py
@@ -4,19 +4,22 @@ import time
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import numpy as np
+from utils import save_image_features
 images_dir = os.path.join(args["static_dir"],args["dataset"])
-data_dir = os.path.join(args["data_dir",args["dataset"]])
+data_dir = os.path.join(args["data_dir"],args["dataset"])
 if not os.path.exists(data_dir):
       os.makedirs(data_dir)
 resnet = args["resnet"]
 feature_model = args["feature_model"]
 def load_images_from_directory(directory,image_size,batch_size=512):
     images = []
+    img_names =[]
     batch = []
     i = 0
     for filename in os.listdir(directory):
 
         st = time.time()
+        img_names.append(filename)
         img = image.load_img(os.path.join(directory, filename), target_size=image_size)
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
@@ -36,7 +39,7 @@ def load_images_from_directory(directory,image_size,batch_size=512):
         features = feature_model.predict(np.asarray(batch))
         images.extend(list(features))
 
-    return np.asarray(images)
-images = load_images_from_directory(images_dir,image_size=(args["img_size"], args["img_size"]))
-with open(os.path.join(data_dir,args["feature_files"]), 'wb') as f:
-    np.save(f, images)
+    return np.asarray(images),img_names
+images,img_names = load_images_from_directory(images_dir,image_size=(args["img_size"], args["img_size"]))
+
+save_image_features(images,img_names,os.path.join(data_dir,args["feature_file"]))
